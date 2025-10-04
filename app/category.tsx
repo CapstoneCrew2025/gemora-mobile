1fc6b8a2c74315fa3c6284a77222ab2343ee2955
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2025-10-04 15:15:44 +0530

    Add category and product components with basic structure

---
+++ app/category.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const category = () => {
+  return (
+    <View>
+      <Text className="text-3xl text-blue-700">category</Text>
+    </View>
+  )
+}
+
+export default category
